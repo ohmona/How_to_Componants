522803a3de912292b993adbfbe574b13eccd2b86
ohmona <66560014+ohmona@users.noreply.github.com>
2020-11-27 19:38:55 +0100

    Delete HowTo_Components.cpp

--- HowTo_Components.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "HowTo_Components.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HowTo_Components, "HowTo_Components" );
